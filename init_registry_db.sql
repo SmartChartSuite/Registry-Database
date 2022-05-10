8be177407521ab8d5faea133be29c898a37d9846
Myung Choi <myungchoi@gmail.com>
2022-05-10 15:29:06 -0400

    adding scripts

---
+++ init_registry_db.sql
@@ -0,0 +1,13 @@
+delete from observation;
+delete from condition_occurrence;
+delete from f_resource_deduplicate;
+delete from flag_info;
+delete from f_person;
+delete from person;
+delete from note;
+delete from location;
+delete from fact_relationship;
+delete from case_info;
+delete from case_log;
+delete from drug_exposure;
+delete from measurement;
