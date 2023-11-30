89b65eef6aa83a2f1422bec3bef65db6d5ee91cf
Myung Choi <myungchoi@gmail.com>
2023-11-30 18:57:42 -0500

    scd registry added

--- omop-registryid-script/init.py
+++ omop-registryid-script/init.py
@@ -8,17 +8,20 @@ import psycopg2.extras as extras
 from configparser import ConfigParser
 from datetime import datetime
 
-name_of_table = "concept"
-
-column_name_map = {'Element OMOP Concept ID' : 'concept_id',
-                   'Element OMOP Concept Name' : 'concept_name',
-                   'Vocabulary' : 'vocabulary',
-                   'Element OMOP Concept Code' : 'concept_code'}
+vocab = "vocab"
+viewer = "viewer"
+name_of_table = "{}.concept".format(vocab)
+column_name_map = {'Element OMOP Concept ID': 'concept_id',
+                   'Element OMOP Concept Name': 'concept_name',
+                   'Vocabulary': 'vocabulary',
+                   'Element OMOP Concept Code': 'concept_code'}
 
 parser = ConfigParser(interpolation=None)
 
+
 def config(filename='config.ini'):
     parser.read(filename)
+
 
 def getConfig(section, inverse=False):
 
@@ -36,6 +39,7 @@ def getConfig(section, inverse=False):
 
     return db
 
+
 def connectdb():
 
     """ Connect to the PostgreSQL database server """
@@ -47,91 +51,93 @@ def connectdb():
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = connect(**params)
-		
+
         # create a cursor
         cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        
-	    # execute a statement
+
+        # execute a statement
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
 
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
-       
-	    # close the communication with the PostgreSQL
+
+        # close the communication with the PostgreSQL
         cur.close()
     except (Exception, DatabaseError) as error:
         print(error)
     return conn
 
-def connectdb1():
+# def connectdb1():
+#
+#     """ Connect to the PostgreSQL database server """
+#     conn = None
+#     try:
+#         # read connection parameters
+#         params = getConfig('postgresql1')
+#
+#         # connect to the PostgreSQL server
+#         print('Connecting to the PostgreSQL database...')
+#         conn = connect(**params)
+#
+#         # create a cursor
+#         cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+#
+# 	    # execute a statement
+#         print('PostgreSQL database version:')
+#         cur.execute('SELECT version()')
+#
+#         # display the PostgreSQL database server version
+#         db_version = cur.fetchone()
+#         print(db_version)
+#
+# 	    # close the communication with the PostgreSQL
+#         cur.close()
+#     except (Exception, DatabaseError) as error:
+#         print(error)
+#     return conn
 
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = getConfig('postgresql1')
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = connect(**params)
-		
-        # create a cursor
-        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        
-	    # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-       
-	    # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, DatabaseError) as error:
-        print(error)
-    return conn
 
 def getNextConcept(cur):
-
     cur.execute("SELECT MAX(concept_id) FROM %s;" % name_of_table)
     max_val = cur.fetchone()[0]
     return max_val + 1
+
 
 def insertConcept(conceptid) :
     # insert into database
     sql = "INSERT INTO %s (" % name_of_table
 
+
 def insertConcept1(conceptid) :
     # insert into database
     sql = "INSERT INTO category ("
 
-def getById(cur, id):
 
+def getById(cur, id):
     cur.execute("SELECT * FROM %s WHERE concept_id = %s;" % (name_of_table, id))
     concept_table = cur.fetchone()
     print('Found: {}'.format(concept_table))
     return concept_table
 
-def getById1(cur, id):
 
-    cur.execute("SELECT * FROM category WHERE concept_id = %s;" % id)
+def getById1(cur, id):
+    cur.execute("SELECT * FROM %s.category WHERE concept_id = %s;" % (viewer, id))
     category_table = cur.fetchone()
     print('Found: {}'.format(category_table))
     return category_table
 
+
 def updateConcept(conn, row):
 
-    sql = "UPDATE concept SET concept_name = '%s', concept_code = '%s', concept_class_id = '%s', vocabulary_id = '%s'  WHERE concept_id = %s" % (str(row['concept_name']),
-    str(row['concept_code']), str(row['concept_class_id']), str(row['vocabulary_id']), row['concept_id'])
+    sql = "UPDATE %s.concept SET concept_name = '%s', concept_code = '%s', concept_class_id = '%s', vocabulary_id = '%s'  WHERE concept_id = %s" % (vocab, str(row['concept_name']), str(row['concept_code']), str(row['concept_class_id']), str(row['vocabulary_id']), row['concept_id'])
     cur.execute(sql)
     conn.commit()
 
 def updateConcept1(conn, row):
 
-    sql = "UPDATE category SET section = '%s', category = '%s', question = '%s' WHERE concept_id = %s" % (row['section'],
+    sql = "UPDATE %s.category SET section = '%s', category = '%s', question = '%s' WHERE concept_id = %s" % (viewer, row['section'],
     row['category'], row['concept_name'], row['concept_id'])
     cur1.execute(sql)
     conn.commit()
@@ -154,7 +160,7 @@ def insertConcept(conn, row):
     # Retreive default table update
     concept = getConfig('default')
     concept['concept_id'] = conceptid
-    concept['concept_name'] = row['concept_name']
+    concept['concept_name'] = row['concept_name'].replace("'", r"''")
     concept['vocabulary_id'] = row['vocabulary_id']
     concept['concept_class_id'] = row['concept_class_id']
     concept['concept_code'] = str(conceptid)
@@ -163,7 +169,7 @@ def insertConcept(conn, row):
     columns = ", ".join(list(concept.keys()))
     values = list(concept.values())
 
-    sql = "INSERT INTO concept (%s) VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (str(columns), values[0], str(values[1]), str(values[2]), str(values[3]), str(values[4]), str(values[5]), str(values[6]), str(values[7]), str(values[8]), str(values[9]))
+    sql = "INSERT INTO %s.concept (%s) VALUES (%s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (vocab, str(columns), values[0], str(values[1]), str(values[2]), str(values[3]), str(values[4]), str(values[5]), str(values[6]), str(values[7]), str(values[8]), str(values[9]))
 
     cur.execute(sql)
     conn.commit()
@@ -172,7 +178,7 @@ def insertConcept(conn, row):
 
 def insertCategory(conn, row):
     cur = conn.cursor()
-    sql = "INSERT INTO category (concept_id, section, category, question) VALUES(%s, '%s', '%s', '%s')" % (row['concept_id'], row['section'], row['category'], row['concept_name'])
+    sql = "INSERT INTO %s.category (concept_id, section, category, question) VALUES(%s, '%s', '%s', '%s')" % (viewer, row['concept_id'], row['section'], row['category'], row['concept_name'].replace("'", r"''"))
     cur.execute(sql)
     conn.commit()
 
@@ -184,14 +190,15 @@ def needChange(row, concept):
     else:
         return False
 
+
 config()
 
 # Create db connection
 conn = connectdb()
 cur = conn.cursor()
 
-conn1 = connectdb1()
-cur1 = conn1.cursor()
+conn1 = connectdb()
+cur1 = conn.cursor()
 
 #get excel config
 e = getConfig('excel')
@@ -200,7 +207,7 @@ e = getConfig('excel')
 wb = pd.read_excel(e['name'], sheet_name=e['sheet'])
 
 # Update columns base on column mapping
-cwb = wb.rename(columns = getConfig('mapping', True))
+cwb = wb.rename(columns=getConfig('mapping', True))
 
 inversecolumn = getConfig('mapping', False)
 
@@ -218,10 +225,11 @@ for index, row in cwb.iterrows():
         modified = True
     else:
         row['concept_id'] = int(row['concept_id'])
-        if type(row['concept_code']) is float:
-            row['concept_code'] = int(row['concept_code'])
-        else:
-            row['concept_code'] = str(row['concept_code'])
+        row['concept_code'] = int(row['concept_code'])
+        # if type(row['concept_code']) is float:
+        #     row['concept_code'] = int(row['concept_code'])
+        # else:
+        #     row['concept_code'] = str(row['concept_code'])
 
         searched = getById(cur, row['concept_id'])
         change = needChange(row, searched)
@@ -236,7 +244,7 @@ for index, row in cwb.iterrows():
         print (inversecolumn['concept_id'])
         wb.loc[index, inversecolumn['concept_id']] = concept['concept_id']
         # wb.loc[index, inversecolumn['concept_name']] = str(concept[1])
-        wb.loc[index, inversecolumn['concept_code']] = str(concept['concept_code'])
+        wb.loc[index, inversecolumn['concept_code']] = int(concept['concept_code'])
         # wb.loc[index, inversecolumn['vocabulary_id']] = str(concept[3])
         # wb.loc[index, inversecolumn['concept_class_id']] = str(concept[4])
         row['concept_id'] = concept['concept_id']
@@ -255,3 +263,7 @@ wb.to_excel(e['name'], index=False)
 if conn is not None:
     conn.close()
     print('Database connection closed.')
+
+if conn1 is not None:
+    conn1.close()
+    print('Database connection1 closed.')
